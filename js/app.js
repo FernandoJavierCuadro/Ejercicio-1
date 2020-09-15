8b263f3bd872777fb93645286f8ef1ef8d817b99
nandopeyton <nandopeyton@users.noreply.github.com>
2020-09-15 09:47:54 -0300

    Primera Subida

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(".nav-link").on("click", function () {
+  $(".arrowRotate", this).css("transform", "rotate(90deg)");
+  console.log("exp");
+});
+
+$(".dropdown").on("hidden.bs.dropdown", function () {
+  $(".arrowRotate", this).css("transform", "rotate(0deg)");
+});
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar.navbar-expand-lg.navbar-dark.fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
