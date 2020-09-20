3e7350011af982120af7af5221abfc880ae418bf
nandopeyton <nandopeyton@users.noreply.github.com>
2020-09-20 13:44:30 -0300

    added comments

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
-$(".nav-link").on("click", function () {
-  $(".arrowRotate", this).css("transform", "rotate(90deg)");
-});
-
-$(".dropdown").on("hidden.bs.dropdown", function () {
-  $(".arrowRotate", this).css("transform", "rotate(0deg)");
-});
+// Navbar - background
 
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".navbar.navbar-expand-lg.navbar-dark.fixed-top");
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
   });
+});
+
+// Navbar - arrows ("Landings" dropdown)
+
+$(".nav-link").on("click", function () {
+  $(".arrowRotate", this).css("transform", "rotate(90deg)");
+});
+
+$(".dropdown").on("hidden.bs.dropdown", function () {
+  $(".arrowRotate", this).css("transform", "rotate(0deg)");
 });
